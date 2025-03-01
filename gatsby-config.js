ee4f114effc0a4f9edfb06531a8504e4efe2fe87
libero <info@blackboxmd.work>
2025-03-01 17:21:07 +0100

    gatsby-config & package.json waren auseinander.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,22 @@
 module.exports = {
   plugins: [
     {
-      resolve: "gatsby-theme-portfolio-minimal",
+      resolve: 'gatsby-theme-portfolio-minimal',
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: 'https://gatsby-starter-portfolio-minimal-theme.netlify.app/', // Used for sitemap generation
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
-          startUrl: "/", // Used in manifest.json
-          backgroundColor: "#FFFFFF", // Used in manifest.json
-          themeColor: "#000000", // Used in manifest.json
-          display: "minimal-ui", // Used in manifest.json
+          favicon: './content/images/favicon.png', // Path is relative to the root
+          siteName: 'My Minimal Portfolio', // Used in manifest.json
+          shortName: 'Portfolio', // Used in manifest.json
+          startUrl: '/', // Used in manifest.json
+          backgroundColor: '#FFFFFF', // Used in manifest.json
+          themeColor: '#000000', // Used in manifest.json
+          display: 'minimal-ui', // Used in manifest.json
         },
-        contentDirectory: "./content",
+        contentDirectory: './content',
         blogSettings: {
-          path: "/blog", // Defines the slug for the blog listing pagegatsby": "^5.14.1",
-          "gatsby-plugin-image": "^3.14.0",
-          "gatsby-source-filesystem": "^3.15.0",
-          "gatsby-theme-portfolio-minimal": "^2.6.4",
-          "gatsby-transformer-remark": "^4.12.0",
-          "react": "^18.2.0",
-          "react-dom": "^18.2.0"
-          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+          path: '/blog', // Defines the slug for the blog listing page
+          usePathPrefixForArticles: true, // Default true (i.e. path will be /blog/first-article)
         },
         // googleAnalytics: {
         //     trackingId: "UA-XXXXXX-X",
